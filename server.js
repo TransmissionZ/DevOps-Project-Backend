7a7d692c22cd0d21e7a558004c56b8f7b8eab901
Haroon <m.haroon.s@pm.me>
2023-05-12 02:28:08 +0500

    added test

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+module.exports = server;
